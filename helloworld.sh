8557c8911748d4712124ff43a23c789183d88d4c
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-09-21 15:58:54 +0530

    arguments practice

--- helloworld.sh
+++ helloworld.sh
@@ -1,14 +1,8 @@
 #!/bin/bas
 
-name=ABHISHEK
-car=tharroxx
+person1=abhi
+person2=shek
 
-echo "what is you name"
-echo "$name is my name"
-echo "what car "
-echo "$car is my car"
-echo "your $car looks great"
-echo "after writing the script in the vscode" 
-echo "and do git add file name then git commit -m file name then git push origin main"
-echo "go to the server and git pull"
-echo "sdjkfuisb"
+echo "his first name is $person1"
+
+echo "his second name is $person2" 
