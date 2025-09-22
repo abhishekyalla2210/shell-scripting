446d8a58071df6b17a340cb62b91ec57f9a97e51
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-09-22 15:58:32 +0530

    arguments practice

--- helloworld.sh
+++ helloworld.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo "everything you passed= $@"
-echo "everything you passed= $*"
-echo "$0"
-echo "$PWD"
-echo "$USER"
-echo "$HOME"
-echo
+START_TIME=$(date %s)
+
+slepp 5
+END_TIME=$(date %S)
+
+TOTAL_TIME=(($END_TIME-$START_TIME))
+
+echo " now the total is:$TOTAL_TIME
