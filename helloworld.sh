f7f412fe591e8e6af4f9d7045506720d36117db0
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-09-23 12:34:13 +0530

    practising

--- helloworld.sh
+++ helloworld.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-START_TIME=100
+LEADER=("TRUMP" "MODI")
 
-
-END_TIME=200
-
-TOTAL_TIME=$(($END_TIME+$START_TIME))
-
-echo " now the total is:$TOTAL_TIME"
+echo "all leader: ${LEADER[@]}"
