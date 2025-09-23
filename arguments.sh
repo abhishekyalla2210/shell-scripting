e645332a6290a78f4f410bc83c62f1531b0ff144
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-09-23 22:26:56 +0530

    arguments practice

--- arguments.sh
+++ arguments.sh
@@ -4,6 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER -lt 20 ]; then
     echo "the number is less than 20"
+elif [ $NUMBER -eq 20]; then
+    echo "it is equal to 20"    
 
 else 
     echo "the number is greater than 20"
