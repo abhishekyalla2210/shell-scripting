43fa5d840268c2ee498724256ee438791115f36d
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-09-25 12:14:27 +0530

    arguments practice

--- installations.sh
+++ installations.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 USERID=$(id -u)
+$R="\e[31m"
+$G="\e[32m"
+$Y="\e[33m"
 
 if [ $USERID -ne 0 ]; then
     echo "it is a failed"
@@ -10,15 +13,17 @@ fi
 
 VALIDATE(){
    if [ $1 -eq 0 ]; then
-    echo "$2 is a success"
+    echo -e "$2 is a $G success $N"
    else
-    echo "$2 is a falure"
+    echo -e "$2 is a $R failure $N"
     exit 1
     
  fi
 }
 
 dnf install mysql -y
+if [ $? -ne 0]; then
+    echo "$2 already exists ....$G skipping $N"
 VALIDATE $? "mysql"
 
 dnf install nginxadf -y
