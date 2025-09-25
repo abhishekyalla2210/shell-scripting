ec06031c7cbbd32e89501fb21e37b787d0310088
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-09-25 08:52:25 +0530

    arguments practice

--- conditions.sh
+++ conditions.sh
@@ -3,7 +3,7 @@
 echo "enter the number"
 read NUMBER
 
-if [ ($NUMBER % 2) -eq 0 ]; then
+if [ $(($NUMBER % 2)) -eq 0 ]; then
     echo "the number $NUMBER is EVEN"
 else
     echo "the number $NUMBER is odd"
