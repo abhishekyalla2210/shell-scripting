62e45da539f608ef5a0ee51ae97e5dbdf18c735b
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-09-25 10:32:06 +0530

    arguments practice

--- installations.sh
+++ installations.sh
@@ -15,3 +15,20 @@ if [ $? -eq 0 ]; then
 else
     echo "mysql is a falure"
 fi
+
+dnf install nginxghj -y
+
+if [ $? -eq 0 ]; then
+    echo "nginx is a success"
+else
+    echo "nginx is a falure"
+     exit 1
+fi
+
+dnf install mongodb-mongosh -y
+
+if [ $? -eq 0 ]; then
+    echo "mongodb-mongosh is a success"
+else
+    echo "mongodb-mongosh is a falure"
+fi
