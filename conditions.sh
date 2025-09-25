88a9e407b350e76f65300b302cbb445266d00789
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-09-25 09:03:53 +0530

    arguments practice

--- conditions.sh
+++ conditions.sh
@@ -3,8 +3,7 @@
 echo "enter the number"
 read NUMBER
 
-if [ $(($NUMBER % 2)) -eq 0 ]; then
-    echo "the number $NUMBER is EVEN"
-else
-    echo "the number $NUMBER is odd"
+if [ $(($NUMBER % 2,3,5 -eq 0 ))];then
+    echo "the given number $NUMBER is a prime number"
+else    echo "it is not prime"
 fi
